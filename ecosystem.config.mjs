5ad929e5c482c1309fd45f82f1a0037396db5960
Amon Benson <amonkbenson@gmail.com>
2024-08-11 14:25:40 +0200

    added ecosystem config

---
+++ ecosystem.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  apps: [
+    {
+      name: "goatless-nuxt",
+      port: 80,
+      exec_mode: "cluster",
+      instances: "max",
+      script: "./output/server/index.mjs"
+    }
+  ]
+};
