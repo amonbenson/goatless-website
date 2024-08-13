c542e9c3539ade7ade916870daa7796845fe1e93
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2024-08-13 12:15:57 +0200

    additional fixes to ecosystem

--- deploy/ecosystem.config.cjs
+++ deploy/ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: "goatless-website",
       port: 80,
       script: "./.output/server/index.mjs",
-      max_memory_restart: "500M",
+      max_memory_restart: "1500M",
       env_production: {
         NODE_ENV: "production",
         PORT: 80,
@@ -16,7 +16,7 @@ module.exports = {
       name: "deploy-webhook",
       port: 8923,
       script: "gunicorn -w 2 -b $HOST:$PORT webhook:app",
-      max_memory_restart: "500M",
+      max_memory_restart: "1500M",
       env_production: {
         PORT: 8923,
         exec_mode: "cluster",
