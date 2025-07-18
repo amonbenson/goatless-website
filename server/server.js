1d8d9e98dbe2486fb069c525998b17c89d4063fd
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2025-07-18 19:07:23 +0200

    fixed certificate reading

--- server/server.js
+++ server/server.js
@@ -56,24 +56,9 @@ app.use(async (ctx) => {
   }
 });
 
-const port = Deno.env.get("PORT") || 80;
-
-const secure = Deno.env.get("SSL_CERT") && Deno.env.get("SSL_KEY");
-if (secure) {
-  const cert = await Deno.readTextFile(Deno.env.get("SSL_CERT"));
-  const key = await Deno.readTextFile(Deno.env.get("SSL_KEY"));
-}
-
-app.addEventListener("listen", () => {
-  console.log(`Server is running on ${secure ? "https" : "http"}://localhost:${Deno.env.get("PORT") || 8080}`);
-  if (secure) {
-    console.log("SSL is enabled");
-  }
-});
-
 await app.listen({
-  port,
+  port: Deno.env.get("PORT") || 80,
   secure,
-  cert,
-  key,
+  cert: Deno.readTextFileSync(Deno.env.get("SSL_CERT")),
+  key: Deno.readTextFileSync(Deno.env.get("SSL_KEY")),
 });
