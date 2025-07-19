129160df0d5d1bd5ed49e39abc1e16d1e04e38f5
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2025-07-19 11:53:02 +0200

    updated autodeploy script

--- autodeploy.sh
+++ autodeploy.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 set -eux
+set -a && source .env && set +a
 
-PIPE_NAME="${PIPE_NAME:-./webhook/pipe}"
+WEBHOOK_PIPE="${WEBHOOK_PIPE:-./webhook/pipe}"
 
 # Create the named pipe if it doesn't exist
-if [[ ! -p "$PIPE_NAME" ]]; then
-  mkfifo "$PIPE_NAME"
+if [[ ! -p "$WEBHOOK_PIPE" ]]; then
+  mkfifo "$WEBHOOK_PIPE"
 fi
 
 while true; do
-  echo "Waiting for trigger on pipe \"${PIPE_NAME}\"..."
+  echo "Waiting for trigger on pipe \"${WEBHOOK_PIPE}\"..."
 
   # Wait for new data in the pipe
-  if read line < "$PIPE_NAME"; then
+  if read line < "$WEBHOOK_PIPE"; then
     echo "Received trigger: $line"
 
     # Stop running containers
