7483674d3bcc1a6df35484f5c78a48a58095bc40
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2025-08-06 17:08:17 +0200

    updated dependencies and background video

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
-import { createHead } from "@unhead/vue";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import App from "./App.vue";
 import "./assets/css/index.css";
 
-gsap.registerPlugin(ScrollTrigger);
-
-const head = createHead();
-const pinia = createPinia();
 const app = createApp(App);
-
-app.use(head);
-app.use(pinia);
 app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
-import icons from "unplugin-icons/vite";
-import iconsResolver from "unplugin-icons/resolver";
-import components from "unplugin-vue-components/vite";
-// import markdown from "unplugin-vue-markdown/vite";
-import vueRouter from "unplugin-vue-router/vite";
 import svgLoader from "vite-svg-loader";
 
 // load vite environment variables
@@ -14,31 +9,9 @@ const viteEnv = loadEnv(process.env.NODE_ENV, process.cwd());
 export default defineConfig({
   base: viteEnv.VITE_BASE_URL,
   plugins: [
-    vueRouter({
-      extensions: [".vue", ".md"],
-      importMode: "async",
-    }),
     vue({
-      include: [/\.vue$/, /\.md$/],
+      include: [/\.vue$/],
     }),
-    // markdown({
-    //   markdownitOptions: {
-    //     html: true,
-    //     linkify: true,
-    //     typographer: true,
-    //   },
-    //   headEnabled: true,
-    //   wrapperComponent: "MarkdownBody",
-    //   wrapperDiv: false,
-    // }),
-    components({
-      extensions: ["vue", "md"],
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      resolvers: [
-        iconsResolver(),
-      ],
-    }),
-    icons(),
     svgLoader(),
   ],
   resolve: {
