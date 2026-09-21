e6932ea0365f4c7004779d0c1a663e6bf30b99a0
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2026-09-21 23:30:14 +0200

    fixed linter issues

--- src/components/MusicSection.vue
+++ src/components/MusicSection.vue
@@ -14,9 +14,11 @@ import Section from "./Section.vue";
   >
     <iframe
       src="https://open.spotify.com/embed/artist/6J58yaHEfHSVAiNlkpdoFp?theme=0"
-      width="100%" height="352" frameborder="0"
+      width="100%"
+      height="352"
+      frameborder="0"
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       loading="lazy"
-    ></iframe>
+    />
   </Section>
 </template>
